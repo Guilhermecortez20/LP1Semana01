c7bcbe9049d2bc000821f15d4e1cd0ba5350ec86
Guilhermecortez20 <Guilhermecortez445@gmail.com>
2025-02-20 22:36:22 +0000

    Add string variables with escape sequences and Unicode

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+          
+        string simpleString = "Olá, mundo!";
+        string stringComAspas = "Ele disse: \"C# é incrível!\"";
+        string stringComNovaLinha = "Linha 1\nLinha 2\nLinha 3";
+        string stringComTab = "Nome\tIdade\tPaís";
+
+        
+        string stringComUnicode = "Símbolos Unicode: \u2665 \u2600 \u263A"; // ♥ ☀ ☺
+        string stringComEmoji = "Emojis: 😀🚀🔥";
+
+        
+        Console.WriteLine("### Strings Simples ###");
+        Console.WriteLine(simpleString);
+        Console.WriteLine(stringComAspas);
+        Console.WriteLine(stringComNovaLinha);
+        Console.WriteLine(stringComTab);
+        Console.WriteLine();
+
+        Console.WriteLine("### Strings com Unicode ###");
+        Console.WriteLine(stringComUnicode);
+        Console.WriteLine(stringComEmoji);
+        }
+    }
+}
