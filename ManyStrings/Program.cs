eadb210d5e2b0093f983da6d97b503f623734d64
Guilhermecortez20 <Guilhermecortez445@gmail.com>
2025-02-20 22:48:58 +0000

    Add string concatenation and interpolation examples

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -16,6 +16,21 @@ namespace ManyStrings
         string stringComUnicode = "Símbolos Unicode: \u2665 \u2600 \u263A"; // ♥ ☀ ☺
         string stringComEmoji = "Emojis: 😀🚀🔥";
 
+        int numero = 2;
+        string letra = "a";
+        string concatenacao1 = letra + numero;    
+        string concatenacao2 = "abc" + numero;    
+
+        string texto = "programação";
+        string concatenacao3 = "Eu gosto de " + texto + "!"; 
+
+        
+        int x = 5;
+        int y = 10;
+        string interpolacao1 = $"O valor de x é {x}";
+        string interpolacao2 = $"{x} mais {y} é igual a {x + y}";
+        string interpolacao3 = $"O dobro de {x} é {x * 2}";
+
         
         Console.WriteLine("### Strings Simples ###");
         Console.WriteLine(simpleString);
@@ -27,6 +42,18 @@ namespace ManyStrings
         Console.WriteLine("### Strings com Unicode ###");
         Console.WriteLine(stringComUnicode);
         Console.WriteLine(stringComEmoji);
+        Console.WriteLine();
+
+        Console.WriteLine("### Concatenação de Strings ###");
+        Console.WriteLine(concatenacao1);
+        Console.WriteLine(concatenacao2);
+        Console.WriteLine(concatenacao3);
+        Console.WriteLine();
+
+        Console.WriteLine("### Interpolação de Strings ###");
+        Console.WriteLine(interpolacao1);
+        Console.WriteLine(interpolacao2);
+        Console.WriteLine(interpolacao3);
         }
     }
 }
