2033701927816a66735c88725c37bc3b09d5a327
Guilhermecortez20 <Guilhermecortez445@gmail.com>
2025-02-20 21:37:59 +0000

    Add XML documentation to Program.cs

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,14 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Classe Principal do programa.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Método principal
+        /// </summary>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
