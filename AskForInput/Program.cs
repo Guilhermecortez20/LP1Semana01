b5acebcf04dc7fe1c1084e6cc530922cc69f2f5f
Guilhermecortez20 <Guilhermecortez445@gmail.com>
2025-02-20 23:51:29 +0000

    Add input handling for integer and real numbers

---
+++ AskForInput/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+             
+        Console.Write("Digite um número inteiro: ");
+        string inputInt = Console.ReadLine();
+        
+        int numeroInteiro = int.Parse(inputInt);
+
+        Console.Write("Digite um número real: ");
+        string inputFloat = Console.ReadLine();
+
+        float numeroReal = float.Parse(inputFloat, CultureInfo.InvariantCulture);
+
+        float soma = numeroInteiro + numeroReal;
+        Console.WriteLine($"A soma de {numeroInteiro} e {numeroReal} é {soma}");
+    
+        }
+    }
+}
