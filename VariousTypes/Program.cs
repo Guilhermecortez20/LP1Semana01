ad6af78bb42cca765fe147623eacffdc71d72608
Guilhermecortez20 <Guilhermecortez445@gmail.com>
2025-02-20 22:01:05 +0000

    Add integer variables and print their values

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+    {
+        byte a = 255;
+        short b = -32768;
+        int c = 2147483647;
+        long d = 9223372036854775807;
+
+        // Imprimir valores das variáveis inteiras
+        Console.WriteLine($"Byte: {a}");
+        Console.WriteLine($"Short: {b}");
+        Console.WriteLine($"Int: {c}");
+        Console.WriteLine($"Long: {d}");
+        }
+    }
+}
